fdf497f654c00b771f4aef7e0fa7b669c1f36c62
VollständigerName <VollstaendigerName@mail.de>
2025-10-12 16:44:39 +0200

    1.0.1
    V 1.0.1:
    Added:
    -Hide when using siege weapons

--- SigilOfAimRectangle/SigilOfAimRectangle.lua
+++ SigilOfAimRectangle/SigilOfAimRectangle.lua
@@ -4,7 +4,7 @@
 --[[
     AddOn Name:         SigilOfAimRectangle
     Description:        Custom dot-style crosshair reticle replacement
-    Version:            1.0.0
+    Version:            1.0.1
     Author:             VollständigerName
     Dependencies:       None
 --]]
@@ -100,9 +100,10 @@ function SigilOfAimRectangleUpdate()
     
     -- Check if player is in mouse mode
     local inMouseMode = IsGameCameraUIModeActive()
+    local inSiegeWeapon = IsPlayerControllingSiegeWeapon()
+        
     if reticleControl then
-        -- Hide custom crosshair in mouse mode
-        reticleControl:SetHidden(inMouseMode)
+        reticleControl:SetHidden(inMouseMode or inSiegeWeapon)
     end
     
     -- Get various game state information for visual feedback
